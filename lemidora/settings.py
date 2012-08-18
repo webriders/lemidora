9f7c4a08c2279d8932e94e8ee6db05a67836326e
Rostyslav Bryzgunov <kottenator@gmail.com>
2012-08-18 14:39:17 +0300

    Setup Heroku & Dev settings

--- lemidora/settings.py
+++ lemidora/settings.py
@@ -20,6 +20,13 @@ DATABASES = {
     }
 }
 
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+}
+
+
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
 # although not all choices may be available on all operating systems.
