c1d2dbcfa0dbf0e2a3961412613006bae517407a
Olexiy Strashko <olexiy.strashko@webriders.com.ua>
2012-08-18 21:41:05 +0300

    Wall & image admin

---
+++ lemidora/apps/main/utils/test_utils.py
@@ -0,0 +1,17 @@
+from django.contrib.auth.models import User
+
+def create_user(name, email=None, password=None, first_name=None, last_name=None):
+    user = User()
+    user.username = name
+    user.first_name = first_name or ""
+    user.last_name = last_name or ""
+    user.set_password(password or '1')
+
+    if email:
+        user.email = email
+    else:
+        user.email = name + '@' + name
+
+    user.save()
+    return user
+
--- lemidora/apps/walls/tests/test_wall_service.py
+++ lemidora/apps/walls/tests/test_wall_service.py
@@ -1,13 +1,22 @@
 from django.test import TestCase
+from main.utils.test_utils import create_user
 from walls.models import Wall
 from walls.services.wall_service import WallService
 
 class TestWallService(TestCase):
     wall_service = WallService()
 
-    def test_create_wall(self):
+    def test_create_anonymous_wall(self):
         wall = self.wall_service.create_wall()
 
         self.assertIsInstance(wall, Wall)
-        self.assertEqual(wall.hash, "")
+        self.assertIsNotNone(wall.hash)
         self.assertEqual(wall.owner, None)
+
+    def test_create(self):
+        user = create_user('goga')
+        wall = self.wall_service.create_wall(user=user)
+
+        self.assertIsInstance(wall, Wall)
+        self.assertIsNotNone(wall.hash)
+        self.assertEqual(wall.owner, user)
