eba0889c4249c8a0de916d82bd8c22593bea3e13
Rostyslav Bryzgunov <kottenator@gmail.com>
2012-08-20 02:53:47 +0300

    Social auth

--- lemidora/apps/accounts/urls.py
+++ lemidora/apps/accounts/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns(
     'accounts.views',
     url(r'dive/$', 'social_login_page', name='login'),
     url(r'dive/complete/$', 'social_login_complete', name='social_login_complete'),
-    url(r'leave/$', logout, name='logout'),
+    url(r'leave/$', logout, {'next_page': '/'}, name='logout'),
 )
