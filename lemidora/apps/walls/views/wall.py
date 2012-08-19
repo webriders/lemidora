9427df168d2b4d1e7e83b1918653bad4ade9fe25
Rostyslav Bryzgunov <kottenator@gmail.com>
2012-08-20 00:41:15 +0300

    Auto-updater implemented

--- lemidora/apps/walls/views/wall.py
+++ lemidora/apps/walls/views/wall.py
@@ -1,6 +1,7 @@
 from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.views.generic import View, TemplateView, RedirectView
+from main.utils.messages_utils import MessagesContextManager
 from walls.facades.wall_facade import WallFacade
 from walls.services.wall_service import WallService
 
@@ -25,7 +26,8 @@ class WallStatusView(View):
 
     def get(self, request, *args, **kwargs):
         wall_key = kwargs.get('wall_id')
-        return HttpResponse(self.facade.get_wall_json(request.user, wall_key), mimetype="application/json")
+        messages_manager = MessagesContextManager()
+        return HttpResponse(self.facade.get_wall_json(request.user, wall_key, messages_manager), mimetype="application/json")
 
 wall_status = WallStatusView.as_view()
 
