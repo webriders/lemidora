ebed0a5959a99300eb74bef85445a264db2482f8
Ashald <xashald@gmail.com>
2012-08-20 02:29:15 +0300

    Additional social auth views

--- lemidora/apps/accounts/urls.py
+++ lemidora/apps/accounts/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'accounts.views',
     url(r'dive/$', 'social_login_page', name='login'),
+    url(r'dive/complete/$', 'social_login_complete', name='social_login_complete'),
 )
