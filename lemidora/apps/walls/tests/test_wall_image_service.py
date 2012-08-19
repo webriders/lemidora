885c944c11a909104b0f5d16ea43c7da3b5ad28d
Ashald <xashald@gmail.com>
2012-08-19 21:03:23 +0300

    TestWallImageService repaired

--- lemidora/apps/walls/tests/test_wall_image_service.py
+++ lemidora/apps/walls/tests/test_wall_image_service.py
@@ -15,10 +15,9 @@ class TestWallImageService(TestCase):
         user = create_user('dojo')
         wall = self.wall_service.create_wall(user)
 
-        image_data = WallImage(image_file=get_django_file('ubuntu_grunge_800x600.jpg'))
-        image_data.wall_id = wall.id
+        image_data = get_django_file('ubuntu_grunge_800x600.jpg')
 
-        image = self.image_service._create_image(user, image_data)
+        image = self.image_service.create_image(user, wall, image_data, 0, 0)
         image = self.image_service.get_image(user, image.id)
 
         self.assertIsInstance(image, WallImage)
@@ -26,7 +25,7 @@ class TestWallImageService(TestCase):
         self.assertEqual(image.updated_by, user)
         self.assertEqual(image.width, WallImageService.DEFAULT_WIDTH)
         self.assertEqual(image.height, 240)
-        # self.assertEqual(image.image_file.name, 'spring_ubuntu_1900x1200.jpg')
+        self.assertTrue('ubuntu_grunge_800x600.jpg' in image.image_file.name)
         self.assertIsInstance(image.thumbnail, ImageFile)
         self.assertTrue(hasattr(image, 'thumbnail'))
         self.assertEqual(image.thumbnail.width, WallImageService.DEFAULT_WIDTH)
@@ -36,9 +35,9 @@ class TestWallImageService(TestCase):
         user = create_user('dojo')
         wall = self.wall_service.create_wall(user)
 
-        image_data = WallImage(image_file=get_django_file('ubuntu_grunge_800x600.jpg'))
-        image_data.wall_id = wall.id
-        old_image = self.image_service._create_image(user, image_data)
+        image_data = get_django_file('ubuntu_grunge_800x600.jpg')
+
+        old_image = self.image_service.create_image(user, wall, image_data, 0, 0)
 
         image_data = WallImage(
             id=old_image.id,
@@ -71,45 +70,21 @@ class TestWallImageService(TestCase):
         self.assertEqual(image.thumbnail.width, 500)
         self.assertEqual(image.thumbnail.height, 700)
 
-    def test_create_images(self):
-        user = create_user('dojo')
-        wall = self.wall_service.create_wall(user)
-
-        image_file_list = [
-            get_django_file('ubuntu_grunge_800x600.jpg'),
-            get_django_file('ubuntu_black_1440x900.jpg'),
-            get_django_file('ubuntu_grunge_800x600.jpg'),
-        ]
-
-        images = self.image_service.create_images(user, wall.hash, image_file_list, 10, 20)
-        self.assertEqual(len(images), 3)
-
-        self.assertEqual(images[0].x, 10)
-        self.assertEqual(images[0].y, 20)
-
-        self.assertEqual(images[1].x, 10 + WallImageService.DEFAULT_X_OFFSET)
-        self.assertEqual(images[1].y, 20 + WallImageService.DEFAULT_Y_OFFSET)
-
-        self.assertEqual(images[2].x, 10 + WallImageService.DEFAULT_X_OFFSET * 2)
-        self.assertEqual(images[2].y, 20 + WallImageService.DEFAULT_Y_OFFSET * 2)
-
     def test_get_geometry(self):
         user = create_user('dojo')
         wall = self.wall_service.create_wall(user)
 
-        image_data = WallImage(image_file=get_django_file('ubuntu_grunge_800x600.jpg'))
-        image_data.wall_id = wall.id
+        image_data = get_django_file('ubuntu_grunge_800x600.jpg')
 
-        image = self.image_service.create_image(user, image_data)
+        image = self.image_service.create_image(user, wall, image_data, 0, 0)
 
         width, height = self.image_service._get_geometry(image.image_file)
         self.assertEqual(width, WallImageService.DEFAULT_WIDTH)
         self.assertIsNone(height)
 
-        image_data = WallImage(image_file=get_django_file('ubuntu_portrait.jpg'))
-        image_data.wall_id = wall.id
+        image_data = get_django_file('ubuntu_portrait.jpg')
 
-        image = self.image_service.create_image(user, image_data)
+        image = self.image_service.create_image(user, wall, image_data, 0,0 )
 
         width, height = self.image_service._get_geometry(image.image_file)
         self.assertEqual(height, WallImageService.DEFAULT_HEIGHT)
